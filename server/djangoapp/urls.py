baf158aedb25c2617895cb104b364c77542c5c1a
L <reagan@krystl.net>
2022-06-04 00:35:37 +0000

    W1-Part 4 Next Deploy

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path(route='about/', view=views.about, name='about'),
     # path for contact us view
+    path(route='contact/', view=views.contact, name='contact'),
 
     # path for registration by Me
     path('registration/', views.registration_request, name='registration'),
